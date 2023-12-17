1876ea90cc8a3d737e33f5910669952ceeca7bb0
Reethika Ramesh <32198189+reethikar@users.noreply.github.com>
2023-12-16 22:33:58 -0500

    Fix missing commit changes
    Changed how IP is logged in the results

--- ripe-atlas/tls-service/tls-service.go
+++ ripe-atlas/tls-service/tls-service.go
@@ -9,6 +9,7 @@ package main
 
 import (
 	"crypto/tls"
+	"encoding/json"
 	"errors"
 	"flag"
 	"io"
@@ -24,7 +25,8 @@ var l = log.New(os.Stderr, "tlssvc: ", log.Ldate|log.Lmicroseconds|log.LUTC|log.
 var ResultsLogger *log.Logger
 
 type ZeroTraceResult struct {
-	DestinationIP	net.IP
+	DestinationIP	string
+	DestinationPort string
 	RTT		int64
 }
 
@@ -73,19 +75,23 @@ func getTCPHandler(config *tls.Config, iface string, port uint16) tcpHandler {
 		}
 		l.Printf("measurement,%s,%d\n", conn.RemoteAddr(), duration.Microseconds())
 
+		dstIP, dstPort, err := net.SplitHostPort(conn.RemoteAddr().String())
+		if err != nil {
+			l.Printf("Error splitting host and port: %s\n",conn.RemoteAddr())
+		}
+
 		ztResults := ZeroTraceResult{
-			DestinationIP: conn.RemoteAddr(),
-			RTT: duration.Microseconds()
+			DestinationIP: dstIP,
+			DestinationPort: dstPort,
+			RTT: duration.Microseconds(),
 			}
 
 		ztObj, err := json.Marshal(ztResults)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
+			l.Printf("Error writing results: measurement,%s,%d\n", conn.RemoteAddr(), duration.Microseconds())
 		}
 		resultString := string(ztObj)
 		ResultsLogger.Println(resultString)
-		
 		tlsConn := tls.Server(conn, config)
 		if err = tlsConn.Handshake(); err != nil {
 			if !errors.Is(err, io.EOF) {
@@ -104,40 +110,33 @@ func main() {
 		keyFile  string
 		iface    string
 		addr     string
-		log      string
+		logfile      string
 		jsonLog  string
 	)
 	flag.StringVar(&certFile, "cert", "", "The TLS server's certificate file.")
 	flag.StringVar(&keyFile, "key", "", "The TLS server's key file.")
 	flag.StringVar(&iface, "iface", "", "The networking interface to use zerotrace for.")
 	flag.StringVar(&addr, "addr", "0.0.0.0:443", "The TLS server's address to listen on.")
-	flag.StringVar(&log, "log", "", "The log file to which stdout is written.")
+	flag.StringVar(&logfile, "log", "", "The log file to which stdout is written.")
 	flag.StringVar(&jsonLog, "jsonResults", "logFile.jsonl", "The file to which results (in JSON) are written.")
 	flag.Parse()
 
 	if certFile == "" || keyFile == "" || iface == "" {
 		l.Fatalf("The flags -cert, -key, and -iface must be provided.")
 	}
-	if log != "" {
-		f, err := os.OpenFile(log, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
+	if logfile != "" {
+		f, err := os.OpenFile(logfile, os.O_RDWR|os.O_CREATE|os.O_APPEND, 0644)
 		if err != nil {
 			l.Fatalf("Error opening log file: %v", err)
 		}
 		defer f.Close()
 		l.SetOutput(io.MultiWriter(os.Stdout, f))
 	}
-	
 	file, err := os.OpenFile(jsonLog, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
 	if err != nil {
 		l.Fatal(err)
 	}
 	ResultsLogger = log.New(file, "", 0)
-	
-	if domain == "" {
-		l.Fatal("Specify domain name by using the -domain flag.")
-	}
-	InfoLogger = log.New(file, "", 0)
-
 
 	addrPort := netip.MustParseAddrPort(addr)
 
